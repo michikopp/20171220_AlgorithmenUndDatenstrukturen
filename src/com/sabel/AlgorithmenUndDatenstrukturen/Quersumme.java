3a8ec36ec3a91d4dbf7092bd4b185ad3bf1ac5e2
Michael Kopp <michi_kopp1@web.de>
2017-12-20 11:10:04 +0100

    Initial commit

---
+++ src/com/sabel/AlgorithmenUndDatenstrukturen/Quersumme.java
@@ -0,0 +1,15 @@
+package com.sabel.AlgorithmenUndDatenstrukturen;
+
+public class Quersumme {
+
+    private int zahl, quersumme;
+
+    public Quersumme(int zahl) {
+        this.zahl = zahl;
+    }
+
+    public int quersummeBerechnen(int zahl) {
+
+      return 0;
+    }
+}
